43c0054c0d4464a5d83b359ca6d9bd76194e159d
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-08-14 18:36:45 +0100

    jo

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,33 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+  def note_contents
+    self.notes.map {|note| note.content }
+  end
+
+  def note_contents=(new_notes)
+    new_notes.each do |new_note|
+      if new_note.length > 0
+        self.notes << Note.create(content: new_note)
+      end
+    end
+  end
+
+end
